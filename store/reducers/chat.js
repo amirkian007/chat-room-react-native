c19457a858809c38627f2b15023c31dd33ecf737
amirkianadl <amircc1200@gmail.com>
2021-03-29 02:28:32 +0430

    SETUP a chat reducer

--- store/reducers/chat.js
+++ store/reducers/chat.js
@@ -1,21 +1,32 @@
-import { SET_MASSAGES,SET_SOCKET} from '../actions/chat';
+import { SET_USERS,SET_SOCKET,SET_ONLINE_USERS,REMOVE_ONLINE_USERS} from '../actions/chat';
 
 const initialState = {
   massages: [],
-  socket: null
+  socket: null,
+  online_users : []
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case SET_MASSAGES:
+    case REMOVE_ONLINE_USERS:
       return {
         ...state,
-        massages: action.massages
+        online_users:state.online_users.filter(item => item !== action.user) 
+      };
+    case SET_ONLINE_USERS:
+      return {
+        ...state,
+        online_users: state.online_users.concat(action.user)
       };
     case SET_SOCKET:
       return {
         ...state,
         socket: action.socket
+      };
+    case SET_USERS:
+      return {
+        ...state,
+        online_users: action.users
       };
     default:
       return state;
