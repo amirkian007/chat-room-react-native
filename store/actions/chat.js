3f4a50eeef42b4a4a03af884d59c4958ad5b1484
amirkianadl <amircc1200@gmail.com>
2021-03-29 02:28:06 +0430

    ADD an action for chats reducers

--- store/actions/chat.js
+++ store/actions/chat.js
@@ -1,2 +1,29 @@
 export const SET_MASSAGES = "SET_MASSAGES";
 export const SET_SOCKET = "SET_SOCKET";
+export const SET_ONLINE_USERS = "SET_ONLINE_USERS";
+export const REMOVE_ONLINE_USERS = "REMOVE_ONLINE_USERS";
+export const SET_USERS = "SET_USERS";
+
+export const setsocket = (socket) => {
+  return (dispatch) => {
+    // dispatch(setLogoutTimer(expiryTime));
+    dispatch({ type: SET_SOCKET, socket: socket});
+  };
+};
+export const setonlineusers = (user) => {
+  return (dispatch) => {
+    dispatch({ type: SET_ONLINE_USERS, user: user});
+  };
+};
+export const removeonlineuser = (user) => {
+
+  return (dispatch) => {
+    dispatch({ type: REMOVE_ONLINE_USERS, user: user});
+  };
+};
+export const setusers = (users) => {
+
+  return (dispatch) => {
+    dispatch({ type: SET_USERS, users: users});
+  };
+};
