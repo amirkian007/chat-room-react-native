ca6e0ca5fa494bccf1b2bafa48cd8cb39e9a7ac7
amirkianadl <amircc1200@gmail.com>
2021-03-30 21:30:59 +0430

    SET new reducers for auth

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,9 +1,13 @@
-import { AUTHENTICATE, LOGOUT, SET_DID_TRY_AL } from '../actions/auth';
+import { AUTHENTICATE, LOGOUT, SET_DID_TRY_AL ,SET_LOADING,SET_ERROR} from '../actions/auth';
 
 const initialState = {
   token: null,
   userId: null,
-  didTryAutoLogin: false
+  name : null,
+  email : null,
+  didTryAutoLogin: false,
+  loading  :false,
+  error : null
 };
 
 export default (state = initialState, action) => {
@@ -12,6 +16,8 @@ export default (state = initialState, action) => {
       return {
         token: action.token,
         userId: action.userId,
+        name: action.name,
+        email: action.email,
         didTryAutoLogin: true
       };
     case SET_DID_TRY_AL:
@@ -23,6 +29,16 @@ export default (state = initialState, action) => {
       return {
         ...initialState,
         didTryAutoLogin: true
+      };
+    case SET_LOADING:
+      return {
+        ...initialState,
+        loading: action.loading
+      };
+    case SET_ERROR:
+      return {
+        ...initialState,
+        error: action.error
       };
     default:
       return state;
