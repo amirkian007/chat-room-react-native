35a770e0fb2017971815ecaf62e592807b493a12
amirkianadl <amircc1200@gmail.com>
2021-03-30 21:34:15 +0430

    ADD new actions for chat

--- store/actions/chat.js
+++ store/actions/chat.js
@@ -3,6 +3,7 @@ export const SET_SOCKET = "SET_SOCKET";
 export const SET_ONLINE_USERS = "SET_ONLINE_USERS";
 export const REMOVE_ONLINE_USERS = "REMOVE_ONLINE_USERS";
 export const SET_USERS = "SET_USERS";
+export const SET_DRAWER = "SET_DRAWER";
 
 export const setsocket = (socket) => {
   return (dispatch) => {
@@ -25,5 +26,11 @@ export const setusers = (users) => {
 
   return (dispatch) => {
     dispatch({ type: SET_USERS, users: users});
+  };
+};
+export const isdrawer_open = (isdrawer_open) => {
+
+  return (dispatch) => {
+    dispatch({ type: SET_DRAWER, isdrawer_open: isdrawer_open});
   };
 };
