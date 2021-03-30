458f4a7394c1f4f564692a894ad6db73e15c3ef1
amirkianadl <amircc1200@gmail.com>
2021-03-30 21:35:01 +0430

    SET name and email in actions

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,26 +1,38 @@
 import { AsyncStorage } from "react-native";
-
-// export const SIGNUP = 'SIGNUP';
-// export const LOGIN = 'LOGIN';
 export const AUTHENTICATE = "AUTHENTICATE";
 export const LOGOUT = "LOGOUT";
 export const SET_DID_TRY_AL = "SET_DID_TRY_AL";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ERROR = "SET_ERROR";
 import axios from "axios";
 let timer;
 
 export const setDidTryAL = () => {
   return { type: SET_DID_TRY_AL };
 };
+export const setloading = (loading) => {
+  return { type: SET_LOADING, loading: loading };
+};
+export const seterror = (error) => {
+  return { type: SET_ERROR, error: error };
+};
 
-export const authenticate = (userId, token, expiryTime) => {
-  return (dispatch) => {
+export const authenticate = (userId, token, name, email) => {
+  return (dispatch) => {  
     // dispatch(setLogoutTimer(expiryTime));
-    dispatch({ type: AUTHENTICATE, userId: userId, token: token });
+    dispatch({
+      type: AUTHENTICATE,
+      userId: userId, 
+      token: token,
+      name: name, 
+      email: email,
+    });
   };
 };
 
 export const signup = (email, password, name) => {
   return async (dispatch) => {
+    dispatch(setloading(true));
     axios
       .put("/auth/signup", {
         name: name,
@@ -28,24 +40,33 @@ export const signup = (email, password, name) => {
         password: password,
       })
       .then((e) => {
-        console.log(e.data);
-        dispatch(
+        console.log(e);
+        dispatch(setloading(false));   
+
+       if(e.status === 201){ dispatch(
           authenticate(
             e.data.userId,
-            e.data.token
+            e.data.token,
+            e.data.name,  
+            e.data.email 
             //  parseInt(e.data.expiresIn) * 1000
           )
         );
         //   const expirationDate = new Date(
         //     new Date().getTime() + parseInt(resData.expiresIn) * 1000
         //   );
-        saveDataToStorage(e.data.token, e.data.userId);
+        saveDataToStorage(
+          e.data.token,
+          e.data.userId,
+          e.data.name,
+          e.data.email
+        );}
       })
       .catch((err) => {
-        console.log(err);
+        dispatch(setloading(false));
+        console.log("error")
+        dispatch(seterror("email has allready been token"))
       });
-
- 
   };
 };
 
@@ -60,15 +81,23 @@ export const login = (email, password) => {
         console.log(res.data);
         dispatch(
           authenticate(
-              res.data.userId,
-              res.data.token
+            res.data.userId,
+            res.data.token,
+            res.data.name,
+            res.data.email
             //  parseInt(resData.expiresIn) * 1000
           )
         );
-        saveDataToStorage(res.data.token, res.data.userId);
+        saveDataToStorage(
+          res.data.token,
+          res.data.userId,
+          res.data.name,
+          res.data.email
+        );
       })
       .catch((err) => {
-        console.log(err);
+        console.log(err)
+        dispatch(seterror("email and password do not match"))
       });
 
     // const expirationDate = new Date(
@@ -97,12 +126,14 @@ const setLogoutTimer = (expirationTime) => {
   };
 };
 
-const saveDataToStorage = (token, userId, expirationDate) => {
+const saveDataToStorage = (token, userId, name, email) => {
   AsyncStorage.setItem(
     "userdata",
     JSON.stringify({
       token: token,
       userId: userId,
+      name: name,
+      email: email,
       //   expiryDate: expirationDate.toISOString()
     })
   );
